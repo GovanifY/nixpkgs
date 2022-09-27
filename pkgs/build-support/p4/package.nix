f5276a211f9be6dd9accbfd5a7751a93a9c56e2a
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2022-09-27 12:56:12 +0200

    p4: skeleton of build tree

---
+++ pkgs/build-support/p4/package.nix
@@ -0,0 +1,74 @@
+{ lib, stdenv }:
+
+{ buildInputs ? []
+, nativeBuildInputs ? []
+, passthru ? {}
+, preFixup ? ""
+, shellHook ? ""
+
+# needed for buildFlags{,Array} warning
+, buildFlags ? ""
+, buildFlagsArray ? ""
+
+, meta ? {}, ... } @ args:
+
+
+with builtins;
+
+let
+
+  # include = list of includes
+  mkInclude = pkgs.lib.concatStringsSep "\n" (map (x: "#include <" + x + ">") include)
+
+
+
+
+  package = stdenv.mkDerivation (
+
+    nativeBuildInputs = [ p4c ] ++ nativeBuildInputs;
+    buildInputs = buildInputs;
+
+    configurePhase = args.configurePhase or ''
+      runHook preConfigure
+
+      runHook postConfigure
+    '';
+
+    buildPhase = args.buildPhase or ''
+      runHook preBuild
+
+      runHook renameImports
+
+      runHook postBuild
+    '';
+
+    doCheck = args.doCheck or false;
+    checkPhase = args.checkPhase or ''
+      runHook preCheck
+      
+      TODO
+
+      runHook postCheck
+    '';
+
+    installPhase = args.installPhase or ''
+      runHook preInstall
+
+      TODO
+
+      runHook postInstall
+    '';
+
+    strictDeps = true;
+
+    enableParallelBuilding = enableParallelBuilding;
+
+    meta = {
+      # TODO: add FPGA support here!
+      platforms = lib.platforms.linux;
+    } // meta;
+  });
+in
+lib.warnIf (buildFlags != "" || buildFlagsArray != "")
+  "Use the `ldflags` and/or `tags` attributes instead of `buildFlags`/`buildFlagsArray`"
+  package
