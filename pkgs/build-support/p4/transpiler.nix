1871cc3a679bf07369462624c889fb37693bd7eb
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2022-10-01 23:28:02 +0200

    p4/transpiler: fix syntax oversight

--- pkgs/build-support/p4/transpiler.nix
+++ pkgs/build-support/p4/transpiler.nix
@@ -12,41 +12,41 @@ let
   # syntax mapper.
   targets_mapping = { v1switch="V1Switch"; };
 
-  p4_attr = lib.evalModules {
+  p4_attr = (lib.evalModules {
     modules = [ ./transpiler-module.nix ];
     args = p4Source;
   }).config;
 
   # include = list of includes
-  mkInclude = include: pkgs.lib.concatStringsSep "\n" (map (x: "#include <" + x + ">") include);
+  mkInclude = include: concatStringsSep "\n" (map (x: "#include <" + x + ">") include);
 
   # headers.typedef = attrset of type and name
-  mkTypedef = typedef: pkgs.lib.concatStringsSep "\n"  (pkgs.lib.imap1 (i: v: "typedef "
+  mkTypedef = typedef: concatStringsSep "\n"  (imap1 (i: v: "typedef "
   + v.type + " " + v.name + ";") typedef);
 
   # headers.const = attrset of type, name and value
-  mkConst = const: pkgs.lib.concatStringsSep "\n"  (pkgs.lib.imap1 (i: v: "const "
+  mkConst = const: concatStringsSep "\n"  (imap1 (i: v: "const "
   + v.type + " " + v.name + " = " + v.value + ";") const);
 
   # headers.struct = attrset of name and content
-  mkStruct = struct: pkgs.lib.concatStringsSep "\n" (pkgs.lib.imap1 (i: v: "struct " +
-  v.name + " {\n" + (pkgs.lib.concatStringsSep "\n" (pkgs.lib.imap1 (i: v: "
+  mkStruct = struct: concatStringsSep "\n" (imap1 (i: v: "struct " +
+  v.name + " {\n" + (concatStringsSep "\n" (imap1 (i: v: "
   " + v.type + "    " + v.name + ";") v.content) ) + "\n}") struct);
 
   # headers.header = attrset of name and content
   # same as struct but limited to bitfield and int
   # TODO: maybe ensure in nix that this constraint is satisfied?
-  mkHeader = header: pkgs.lib.concatStringsSep "\n" (pkgs.lib.imap1 (i: v: (if
+  mkHeader = header: concatStringsSep "\n" (imap1 (i: v: (if
   (header.union) then "header_union " else "header ") +
-  v.name + " {\n" + (pkgs.lib.concatStringsSep "\n" (pkgs.lib.imap1 (i: v: "
+  v.name + " {\n" + (concatStringsSep "\n" (imap1 (i: v: "
   " + v.type + "    " + v.name + ";") v.content) ) + "\n}") header);
 
   # headers.enum = attrset of name and content
-  mkEnum = enum: pkgs.lib.concatStringsSep "\n"  (pkgs.lib.imap1 (i: v: "enum " +
-  v.name + " { " + (pkgs.lib.concatStringsSep ", " v.content) + " };") enum);
+  mkEnum = enum: concatStringsSep "\n"  (imap1 (i: v: "enum " +
+  v.name + " { " + (concatStringsSep ", " v.content) + " };") enum);
 
   # headers.error = list of possible errors
-  mkError = error: "error { " + (pkgs.lib.concatStringsSep ", " error) + " };";
+  mkError = error: "error { " + (concatStringsSep ", " error) + " };";
 
 
 
@@ -61,7 +61,7 @@ let
   # TODO: add v1switch and other targets there!
   #mkTarget =  then
 
-  mkCallStack = call_stack: pkgs.lib.removeSuffix ",\n" (pkgs.lib.concatStringsSep "(),\n" call_stack);
+  mkCallStack = call_stack: removeSuffix ",\n" (concatStringsSep "(),\n" call_stack);
 
   # XXX: I am assuming that if you want to setup a target you want it to be the
   # main logic, does this assumption always holds true?
